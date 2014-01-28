558c85e8cba6d7bcf5c608a233790d1dcc561ff3
rodrigochaves <rodrigochweb@gmail.com>
2014-01-28 13:12:07 -0200

    Criação do projeto

---
+++ imobiliaria/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imobiliaria::Application.config.secret_key_base = 'f9cc918f948ee43c49d45d1ab497458c321ac0fdf3e52908e8792b8a9c1b50991ffbd5e80b20191510d75a73b59934b205fe70b3c8b70ba4afeb4c6fa976d6e9'
---
+++ imobiliaria/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Imobiliaria::Application.config.session_store :cookie_store, key: '_imobiliaria_session'
